2edb285b79a46e2f1165c6e538a9d868e7776779
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2025-08-13 15:03:53 +0530

    Implemented : Theme Error fix

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,7 +1,9 @@
 
 'use client';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/ui/card";
-import { DollarSign, Users, CreditCard, Activity, ShoppingCart, UserX } from 'lucide-react';
+// import { DollarSign, Users, CreditCard, Activity, ShoppingCart, UserX } from 'lucide-react';
+import { FaDollarSign, FaUsers, FaCreditCard, FaShoppingCart } from "react-icons/fa";
+import {FiUsers, FiActivity} from "react-icons/fi";
 import { SalesChart } from "@/components/charts/SalesChart";
 import { RevenueChart } from "@/components/charts/RevenueChart";
 import { SalesBySalonChart } from "@/components/charts/SalesBySalonChart";
@@ -28,7 +30,7 @@ export default function AdminPage() {
             <CardTitle className="text-sm font-medium">
               Total Revenue
             </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
+            <FaDollarSign className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">$45,231.89</div>
@@ -42,7 +44,7 @@ export default function AdminPage() {
             <CardTitle className="text-sm font-medium">
               Total Bookings
             </CardTitle>
-            <ShoppingCart className="h-4 w-4 text-muted-foreground" />
+            <FaShoppingCart className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+2350</div>
@@ -54,7 +56,7 @@ export default function AdminPage() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Customers</CardTitle>
-            <Users className="h-4 w-4 text-muted-foreground" />
+            <FaUsers className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+12,234</div>
@@ -68,7 +70,7 @@ export default function AdminPage() {
             <CardTitle className="text-sm font-medium">
               Total Vendors
             </CardTitle>
-            <Activity className="h-4 w-4 text-muted-foreground" />
+            <FiActivity className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+573</div>
