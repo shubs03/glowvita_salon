27c6ef61099aa313c16f12b2249550f4add03bdf
Manish Sonawane <manishsonawane3010@gmail.com>
2025-08-20 10:29:40 +0000

    please transfer the marquee related changes from config file to global c

--- apps/crm/tailwind.config.ts
+++ apps/crm/tailwind.config.ts
@@ -11,41 +11,4 @@ const config: Pick<Config, "content" | "presets"> = {
   presets: [sharedConfig],
 };
 
-// Add animation for marquee
-const extendedConfig = {
-    ...config,
-    theme: {
-        ...sharedConfig.theme,
-        extend: {
-            ...sharedConfig.theme.extend,
-            animation: {
-                ...sharedConfig.theme.extend.animation,
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
-                marquee: 'marquee 40s linear infinite',
-                marquee2: 'marquee2 40s linear infinite',
-            },
-            keyframes: {
-                ...sharedConfig.theme.extend.keyframes,
-                'accordion-down': {
-                    from: { height: '0' },
-                    to: { height: 'var(--radix-accordion-content-height)' },
-                },
-                'accordion-up': {
-                    from: { height: 'var(--radix-accordion-content-height)' },
-                    to: { height: '0' },
-                },
-                marquee: {
-                    '0%': { transform: 'translateX(0%)' },
-                    '100%': { transform: 'translateX(-100%)' },
-                },
-                marquee2: {
-                    '0%': { transform: 'translateX(100%)' },
-                    '100%': { transform: 'translateX(0%)' },
-                },
-            },
-        },
-    },
-};
-
-export default extendedConfig;
+export default config;
