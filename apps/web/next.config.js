9727ab84b2934b6d0abc9970a689cba202c06fce
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2025-10-06 12:57:38 +0530

    Fixed : Build Error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,15 @@ require('dotenv').config({ path: '../../.env' });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SECRET_USER: process.env.JWT_SECRET_USER,
+    JWT_SECRET_ADMIN: process.env.JWT_SECRET_ADMIN,
+    JWT_SECRET_VENDOR: process.env.JWT_SECRET_VENDOR,
+    JWT_SECRET_DOCTOR: process.env.JWT_SECRET_DOCTOR,
+    JWT_SECRET_SUPPLIER: process.env.JWT_SECRET_SUPPLIER,
+    // Add other environment variables as needed
+  },
   async headers() {
     return [
       {
