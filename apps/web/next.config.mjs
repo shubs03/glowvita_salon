90f88527080ab403334c4ff04cefbc77e894ca1d
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2026-01-16 17:04:26 +0530

    Fixed : Build Error

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -6,10 +6,11 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/ui", "@repo/lib", "@repo/store"],
+  transpilePackages: ["@repo/ui", "@repo/lib", "@repo/store", "@repo/config"],
   webpack: (config) => {
     config.resolve.alias['@repo/lib'] = path.resolve(__dirname, '../packages/lib/src');
     config.resolve.alias['@repo/store'] = path.resolve(__dirname, '../packages/store/src');
+    config.resolve.alias['@repo/config'] = path.resolve(__dirname, '../packages/config');
     return config;
   },
 };
