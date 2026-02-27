a519ec12456475bf99bd3a52e8e08ae9d99debd0
juili <juiware30@gmail.com>
2026-02-27 10:34:02 +0530

    implemnt:supplier orders in admin

--- packages/store/src/services/api.js
+++ packages/store/src/services/api.js
@@ -998,6 +998,21 @@ export const glowvitaApi = createApi({
       invalidatesTags: ["Vendor"],
     }),
 
+    getSupplierOrders: builder.query({
+      query: (params = {}) => {
+        const queryParams = new URLSearchParams();
+        if (params.regionId && params.regionId !== 'all') queryParams.append('regionId', params.regionId);
+        if (params.status && params.status !== 'all') queryParams.append('status', params.status);
+        const queryString = queryParams.toString();
+        return {
+          url: `/admin/supplier-orders${queryString ? `?${queryString}` : ""}`,
+          method: "GET"
+        };
+      },
+      providesTags: ["Order"],
+      transformResponse: (response) => (response && response.success ? response.data || [] : []),
+    }),
+
     // Supplier Endpoints
     getSuppliers: builder.query({
       query: (regionId) => {
@@ -3050,6 +3065,7 @@ export const {
   useUpdateDoctorMutation,
   useDeleteDoctorMutation,
   useGetSuppliersQuery,
+  useGetSupplierOrdersQuery,
   useCreateSupplierMutation,
   useRegisterSupplierMutation,
   useUpdateSupplierMutation,
