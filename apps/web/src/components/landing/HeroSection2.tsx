3853f0d20f0336e9ff6592db75c08cce4bedca2b
Siddhi Shinde <siddhi.shinde@ftcash.com>
2026-07-03 15:21:00 +0530

    fixed : herosection of main landing page

--- apps/web/src/components/landing/HeroSection2.tsx
+++ apps/web/src/components/landing/HeroSection2.tsx
@@ -272,7 +272,7 @@ const HeroSection2 = () => {
   }, [categoriesData]);
 
   return (
-    <div className="relative w-full h-[602px] overflow-hidden">
+    <div className="relative w-full min-h-[500px] h-auto lg:h-[700px] overflow-hidden">
       {/* Solid dark maroon base */}
       <div className="absolute inset-0" style={{ backgroundColor: "#422A3C" }} />
 
